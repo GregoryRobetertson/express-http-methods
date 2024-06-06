9dd11a3921274c67c44f8b664c181d28fcd92efb
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-06-06 19:12:03 +0000

    updated

--- index.js
+++ index.js
@@ -18,4 +18,22 @@ res.send('This is the about page.')
 
 app.post('/contact',( req, res)=> {
     const { name, email, message } = req.body;
+res.json({message: ' Your message has been received.'})
+})
+
+app.get('/user/:id', (req, res) => {
+    const userID = req.params.id;
+    res.send(`user ID: ${userID}`)
+})
+
+app.use((req, res) => {
+    res.status(404).send('404 - Not Found');
+})
+
+app.get('/search', (req, res) => {
+    const {q} = req.query;
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
